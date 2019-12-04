8de1fa895bcb9743a5412b12998831c2f3acb90a
Nikita Kapusta <themisterrioo@gmail.com>
2019-12-04 12:01:58 +0200

    create project structure + add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+with open("README.md", 'r') as f:
+    long_description = f.read()
+setup(
+    name='Department Handler',
+    version='1.0',
+    author='Nikita Kapusta',
+    author_email='themisterrioo@gmail.com',
+    long_description=long_description,
+    packages=['department-app'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['Flask', 'flask-sqlalchemy', 'mysql-connector']
+)
